26d22e12f183628c47f98fdcd4b404a36a6a48e4
Jin Su Park <jinsupark@icloud.com>
2021-06-25 02:15:34 -0400

    update 404

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,23 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(pluginTOC);
 
+  const fs = require("fs");
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
   // https://www.11ty.dev/docs/copy/
   eleventyConfig.addPassthroughCopy("*.ico");
